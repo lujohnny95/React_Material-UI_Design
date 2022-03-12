fcbdee4a9daf227b407fd12050d2890f4c8542dc
lujohnny95 <lu_johnny95@yahoo.com>
2022-03-12 21:55:12 +0000

    responsive left sidebar added

--- src/components/Leftbar.js
+++ src/components/Leftbar.js
@@ -1,6 +1,5 @@
-import { ThemeContext } from '@emotion/react';
 import { Typography, Container, makeStyles } from '@material-ui/core';
-import { Home } from '@material-ui/icons';
+import { Bookmark, ExitToApp, Home, Person, PhotoCamera, PlayCircleOutline, Settings, Storefront, TabletMac } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -23,7 +22,14 @@ const useStyles = makeStyles((theme) => ({
       cursor: "pointer"
     },
   },
+  icon: {
+    marginRight: theme.spacing(1),
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "18px",
+    },
+  },
   text: {
+    fontWeight: 500,
     [theme.breakpoints.down("sm")]: {
       display: "none"
     },
@@ -40,16 +46,36 @@ function Leftbar() {
         <Typography className={classes.text}>Homepage</Typography>
       </div>
       <div className={classes.item}>
-        <Home className={classes.icon}/>
-        <Typography className={classes.text}>Homepage</Typography>
+        <Person className={classes.icon}/>
+        <Typography className={classes.text}>Friends</Typography>
       </div>
       <div className={classes.item}>
-        <Home className={classes.icon}/>
-        <Typography className={classes.text}>Homepage</Typography>
+        <PhotoCamera className={classes.icon}/>
+        <Typography className={classes.text}>Snapshots</Typography>
       </div>
       <div className={classes.item}>
-        <Home className={classes.icon}/>
-        <Typography className={classes.text}>Homepage</Typography>
+        <PlayCircleOutline className={classes.icon}/>
+        <Typography className={classes.text}>Videos</Typography>
+      </div>
+      <div className={classes.item}>
+        <TabletMac className={classes.icon}/>
+        <Typography className={classes.text}>Apps</Typography>
+      </div>
+      <div className={classes.item}>
+        <Bookmark className={classes.icon}/>
+        <Typography className={classes.text}>Archived</Typography>
+      </div>
+      <div className={classes.item}>
+        <Storefront className={classes.icon}/>
+        <Typography className={classes.text}>MarketPlace</Typography>
+      </div>
+      <div className={classes.item}>
+        <Settings className={classes.icon}/>
+        <Typography className={classes.text}>Settings</Typography>
+      </div>
+      <div className={classes.item}>
+        <ExitToApp className={classes.icon}/>
+        <Typography className={classes.text}>Logout</Typography>
       </div>
     </Container>
   );
