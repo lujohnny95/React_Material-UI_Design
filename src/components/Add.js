fabe9cc2800fb281d0f27fbc0fae06462f37184d
lujohnny95 <lu_johnny95@yahoo.com>
2022-03-13 21:35:45 +0000

    rightbar and snackbar added

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,5 +1,6 @@
-import { makeStyles, Tooltip, Fab } from "@material-ui/core";
+import { makeStyles, Tooltip, Fab, FormLabel, Button, Container, TextField, Modal, MenuItem, Radio, RadioGroup, FormControlLabel } from "@material-ui/core";
 import { Add as AddIcon } from "@material-ui/icons";
+import MuiAlert from "@material-ui/lab/Alert";
 
 const useStyles = makeStyles((theme) => ({
     fab: {
@@ -20,13 +21,32 @@ const useStyles = makeStyles((theme) => ({
         [theme.breakpoints.down("sm")]: {
             height: "100vh",
             width: "100vw",
-        }
-    }
+        },
+    },
+    form: {
+        padding: theme.spacing(2),
+    },
+    item: {
+        marginBottom: theme.spacing(3),
+    },
 }));
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 function Add() {
     const classes = useStyles();
     const [open, setOpen] = useStyles(false);
+    const [openAlert, setOpenAlert] = useState(false);
+
+    const handleClose = (event, reason) => {
+        if (reason === "clickaway") {
+            return;
+        }
+        
+        setOpenAlert(false);
+    };
 
     return (
     <>
@@ -37,9 +57,84 @@ function Add() {
         </Tooltip>
         <Modal open={open}>
             <Container className={classes.container}>
-
+                <form className={classes.form} autoComplete="off">
+                    <div className={classes.item}>
+                        <TextField 
+                            id="standard-basic" 
+                            label="Title" 
+                            size="small" 
+                            style={{ width: "100%" }}
+                        />
+                    </div>
+                    <div className={classes.item}>
+                        <TextField 
+                            id="outlined-multiline-static"
+                            multiline
+                            rows={5}
+                            defaultValue="Tell your story..."
+                            variant="outlined"
+                            label="Description" 
+                            size="small" 
+                            style={{ width: "100%" }}
+                        />
+                    </div>
+                    <div className={classes.item}>
+                        <TextField select label="Visibility" value="Public">
+                            <MenuItem value="Public">Public</MenuItem>
+                            <MenuItem value="Private">Private</MenuItem>
+                            <MenuItem value="Unlisted">Unlisted</MenuItem>
+                        </TextField>
+                    </div>
+                    <div className={classes.item}>
+                        <FormLabel component="legend">Who can comment?</FormLabel>
+                        <RadioGroup>
+                            <FormControlLabel 
+                                value="Everybody"
+                                label="Everybody"
+                                control={<Radio size="small" />}
+                            />
+                             <FormControlLabel 
+                                value="Friends Only"
+                                label="Friends Only"
+                                control={<Radio size="small" />}
+                            />
+                             <FormControlLabel 
+                                value="Only Me"
+                                label="Only Me"
+                                control={<Radio size="small" />}
+                            />
+                        </RadioGroup>
+                    </div>
+                    <div className={classes.item}>
+                        <Button
+                            variant="outlined"
+                            color="primary"
+                            style={{ marginRight: 20 }}
+                            onClick={() => setOpenAlert(true)}
+                        >
+                            Create
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            color="secondary"
+                            onClick={() => setOpen(false)}
+                        >
+                            Close
+                        </Button>
+                    </div>
+                </form>
             </Container>
         </Modal>
+        <Snackbar
+            open={openAlert}
+            autoHideDuration={4000}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        >
+        <Alert onClose={handleClose} severity="success">
+            This is a success message!
+        </Alert>
+      </Snackbar>
     </>
     );
 };
--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -1,15 +1,91 @@
-import { makeStyles } from '@mui/material';
-import { theme } from './theme';
+import { ClassNames } from '@emotion/react';
+import { ImageList, ImageListItem, Typography, Avatar, makeStyles, AvatarGroup, Divider } from '@material-ui/core';
 
-const useStyles = makeStyles({
-
-});
+const useStyles = makeStyles((theme) => ({
+  container: {
+    paddingTop: theme.spacing(10),
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 500,
+    color: "#555",
+  },
+  link: {
+    marginRight: theme.spacing(2),
+    color: "#555",
+    fontSize: 16,
+  },
+}));
 
 function Rightbar() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      
-    </div>
+    <Container className={ClassNames.container}>
+      <Typography gutterBottom className={classes.title}>Online Friends</Typography>
+      <AvatarGroup max={6} style={{ marginBottom: 20 }}>
+        <Avatar
+          alt="Remy Sharp"
+          src="https://material-ui.com/static/images/avatar/1.jpg"
+        />
+        <Avatar
+          alt="Travis Howard"
+          src="https://material-ui.com/static/images/avatar/2.jpg"
+        />
+        <Avatar
+          alt="Cindy Baker"
+          src="https://material-ui.com/static/images/avatar/3.jpg"
+        />
+        <Avatar alt="Agnes Walker" src="" />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://material-ui.com/static/images/avatar/6.jpg"
+        />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://material-ui.com/static/images/avatar/7.jpg"
+        />
+        <Avatar
+          alt="Trevor Henderson"
+          src="https://material-ui.com/static/images/avatar/8.jpg"
+        />
+      </AvatarGroup>
+      <Typography gutterBottom className={classes.title}>Gallery</Typography>
+        <ImageList rowHeight={100} className={classes.ImageList} cols={2}>
+          <ImageListItem>
+            <img src="https://material-ui.com/static/images/image-list/breakfast.jpg" alt="gallery-image" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="https://material-ui.com/static/images/image-list/morning.jpg" alt="gallery-image" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="https://material-ui.com/static/images/image-list/camera.jpg" alt="gallery-image" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="https://material-ui.com/static/images/image-list/hats.jpg" alt="gallery-image" />
+          </ImageListItem>
+        </ImageList>
+        <Typography gutterBottom className={classes.title}>Categories</Typography>
+        <Link className={classes.link} href="#" variant="body2">
+          Food
+        </Link>
+        <Link className={classes.link} href="#" variant="body2">
+          Travel
+        </Link>
+        <Link className={classes.link} href="#" variant="body2">
+          Lifestyle
+        </Link>
+        <Divider flexItem style={{ marginBottom: 5 }} />
+        <Link className={classes.link} href="#" variant="body2">
+          Social
+        </Link>
+        <Link className={classes.link} href="#" variant="body2">
+          Music
+        </Link>
+        <Link className={classes.link} href="#" variant="body2">
+          Entertainment
+        </Link>
+    </Container>
   );
 }
 
